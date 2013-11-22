ed7f0e716adddba4ef288400e4d645bbef0da0b9
Guillaume Hain <zedtux@zedroot.org>
2013-11-22 13:50:52 +0100

    Updated the secret_token initializer in order to log which key is used

--- config/initializers/secret_token.rb
+++ config/initializers/secret_token.rb
@@ -9,6 +9,10 @@
 
 # Make sure your secret_key_base is kept private
 # if you're sharing your code publicly.
-BrewformulasOrg::Application.config.secret_key_base = AppConfig.secret_key_base rescue "552573ba7d00afb0b67dad7532bb78b7492e02f1573011c15fd4f5b979ec266932c6b13b42c146eec993284a1124eb288334b58d8362f9b8a5ec47ac5b0d2e63"
-
-# In development, test and cucumber environment use the old secret_key_base
+if secret_key_base = AppConfig.secret_key_base rescue nil
+  Rails.logger.info "Using production secret_key_base"
+else
+  Rails.logger.info "Using default secret_key_base"
+end
+secret_key_base = "552573ba7d00afb0b67dad7532bb78b7492e02f1573011c15fd4f5b979ec266932c6b13b42c146eec993284a1124eb288334b58d8362f9b8a5ec47ac5b0d2e63" unless secret_key_base
+BrewformulasOrg::Application.config.secret_key_base = secret_key_base
