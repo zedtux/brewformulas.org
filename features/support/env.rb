2c205b3d9523730fbd09db197b59f75620e01d36
Guillaume Hain <zedtux@zedroot.org>
2013-11-30 18:03:24 +0100

    Use coveralls wear_merged! instead of wear!

--- features/support/env.rb
+++ features/support/env.rb
@@ -7,7 +7,7 @@
 require 'cucumber/rails'
 require 'cucumber/timecop'
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear_merged!
 
 # Capybara defaults to CSS3 selectors rather than XPath.
 # If you'd prefer to use XPath, just uncomment this line and adjust any
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -6,7 +6,7 @@ require 'email_spec'
 require 'rspec/autorun'
 
 require 'coveralls'
-Coveralls.wear!
+Coveralls.wear_merged!
 
 # Requires supporting ruby files with custom matchers and macros, etc,
 # in spec/support/ and its subdirectories.
