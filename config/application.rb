eaff670964039c0c28666e0a0fd4d3db50ae942c
Guillaume Hain <zedtux@zedroot.org>
2013-12-02 19:55:49 +0100

    Fixed path to the fonts folder

--- config/application.rb
+++ config/application.rb
@@ -59,6 +59,6 @@ module BrewformulasOrg
     # Version of your assets, change this if you want to expire all your assets
     config.assets.version = '1.0'
 
-    config.assets.paths << "#{Rails}/app/assets/fonts"
+    config.assets.paths << Rails.root.join("app", "assets", "fonts", "bootstrap")
   end
 end
