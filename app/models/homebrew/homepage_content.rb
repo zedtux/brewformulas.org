2c20a20980c27e2ccb5fd631f216cb69094d4ab6
Guillaume Hain <zedtux@zedroot.org>
2013-12-07 16:22:08 +0100

    Updated Homebrew::HomepageContent documentation

--- app/models/homebrew/homepage_content.rb
+++ app/models/homebrew/homepage_content.rb
@@ -1,3 +1,6 @@
+require "open-uri"
+require "open_uri_redirections"
+
 #
 # Get the HTML code from the home page
 # of a Homebrew formula, do some cleaning
@@ -5,20 +8,23 @@
 #
 # @author [guillaumeh]
 #
-require "open-uri"
-require "open_uri_redirections"
-
 module Homebrew
   class HomepageContent
     def initialize(formula)
       @formula = formula
     end
 
+    #
+    # Fetch the given formula's home page
+    # remove all Hyphen if any
+    # and convert to UTF-8
+    #
+    # @return [String] Formula homepage content
     def fetch
       # Load homepage HTML code
       page_content = open(@formula.homepage, allow_redirections: :all).read
       page_content.gsub!(/&shy;/, "") # Remove all Hyphen
-      page_content = page_content.encode(
+      page_content.encode(
         "UTF-8",
         invalid: :replace,
         undef: :replace,
