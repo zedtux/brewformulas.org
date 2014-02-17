1adbda881fa939035ebb6feacf647fe7a82fd1a7
Guillaume Hain <zedtux@zedroot.org>
2014-02-17 20:36:46 +0100

    Add newrelic to the deployment

--- config/deploy.rb
+++ config/deploy.rb
@@ -23,7 +23,7 @@ set :deploy_to, '/var/www/brewformulas.org'
 # set :pty, true
 
 # Default value for :linked_files is []
-set :linked_files, %w{config/database.yml}
+set :linked_files, %w{config/database.yml config/newrelic.yml}
 
 # Default value for linked_dirs is []
 # set :linked_dirs, %w{bin log tmp/pids tmp/cache tmp/sockets vendor/bundle public/system}
