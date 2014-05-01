7342b044cd996ef0b176feb2e7496ab1ff1207ab
Guillaume Hain <zedtux@zedroot.org>
2014-05-01 19:27:36 +0200

    Update the capistrano lock

--- config/deploy.rb
+++ config/deploy.rb
@@ -1,5 +1,5 @@
-# config valid only for Capistrano 3.1
-lock '3.1.0'
+# config valid only for Capistrano 3.2
+lock '3.2.1'
 
 set :application, 'brewformulas.org'
 set :repo_url, 'https://github.com/zedtux/brewformulas.org.git'
