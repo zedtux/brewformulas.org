4b3dddd69da2ab88b09106a4d29cb681b32ddc31
Guillaume Hain <zedtux@zedroot.org>
2017-06-02 20:30:08 +0200

    Create dependencies_controller.rb

---
+++ app/controllers/dependencies_controller.rb
@@ -0,0 +1,23 @@
+class DependenciesController < ApplicationController
+  include Pres::Presents
+
+  before_action :current_object, only: :index
+
+  def index
+    @presented_formula = present(@formula)
+  end
+
+  private
+
+  def current_object
+    @formula = Homebrew::Formula.find_by!(name: params[:formula_id])
+  rescue ActiveRecord::RecordNotFound
+    respond_to do |format|
+      format.html do
+        flash[:error] = 'This formula doesn\'t exists'
+        redirect_to root_url
+      end
+      format.json { render json: {}, status: :not_found }
+    end
+  end
+end
